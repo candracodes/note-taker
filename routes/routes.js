20364d2b9806ad92bc44b001ab96d9607a94830a
Candra Fried <candracodes@gmail.com>
2021-10-19 10:35:50 -0500

    Created a server.js file with some basic logic and plans on how to move forward. Also created a routes folder to use later for module exports.

---
+++ routes/routes.js
@@ -0,0 +1,1 @@
+// TODO: Connect all the necessary API route using module exports
---
+++ server.js
@@ -0,0 +1,12 @@
+// TODO: REQUIRE NECESSARY DEPENDANCIES
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+// TODO: INITIALIZE EXPRESS & PORT
+
+// TODO: SETUP MIDDLEWARE FOR PARSING JSON & URLENCODED DATA
+
+// TODO: GET ROUTES
+
+// TODO: SETUP LISTENER
